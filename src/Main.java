921bd8dbe0db9c4d918a5a1bade80383ff946767
DjGuruge <djguruge@hotmail.it>
2025-02-17 03:06:54 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+//Crea una classe User che abbia i parametri privati nome ed età e i
+//getter e setter
+//All'interno di User ci sarà anche una funzione che stampi a video le informazioni
+//di età e il nome.
+//Creare 2 oggetti User di cui creerà un'istanza.
+//Del primo si stamperanno le informazioni di default, del secondo si cambieranno le
+//informazioni e poi si stamperanno a video
+
+public class Main {
+    public static void main(String[] args) {
+        User user0 = new User();
+        user0.stampaInfo();
+        User user1 = new User("Marco Polo",29);
+        user1.stampaInfo();
+    }
+}
---
+++ src/User.java
@@ -0,0 +1,39 @@
+//Crea una classe User che abbia i parametri privati nome ed età e i
+//getter e setter
+//All'interno di User ci sarà anche una funzione che stampi a video le informazioni
+//di età e il nome.
+
+public class User {
+    private String nome;
+    private Integer eta;
+
+    public User(String nome, Integer eta ) {
+        this.eta = eta;
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getEta() {
+        return eta;
+    }
+
+    public void setEta(Integer eta) {
+        this.eta = eta;
+    }
+    public User(){
+        this.nome = null;
+        this.eta = null;
+    }
+
+    public void stampaInfo(){
+        System.out.println("il nome é :"+this.nome+" e l'etá é di :"+this.eta);
+    }
+
+}
